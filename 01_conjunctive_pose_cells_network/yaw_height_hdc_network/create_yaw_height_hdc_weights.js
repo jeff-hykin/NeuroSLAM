7fb69a9416dcea560939dfd80dfe5a4b2ef75ed8
Jeff Hykin <jeff.hykin@gmail.com>
2025-02-05 16:58:18 -0600

    formatting

--- 01_conjunctive_pose_cells_network/yaw_height_hdc_network/create_yaw_height_hdc_weights.js
+++ 01_conjunctive_pose_cells_network/yaw_height_hdc_network/create_yaw_height_hdc_weights.js
@@ -1,4 +1,6 @@
 export function createYawHeightHdcWeights(yawDim, heightDim, yawVar, heightVar) {
+    // Creates a 2D normalised distributio of size dim^2 
+
     // Calculate the centers for yaw and height dimensions
     const yawDimCentre = Math.floor(yawDim / 2) + 1
     const heightDimCentre = Math.floor(heightDim / 2) + 1
@@ -10,7 +12,25 @@ export function createYawHeightHdcWeights(yawDim, heightDim, yawVar, heightVar) 
     for (let h = 0; h < heightDim; h++) {
         for (let y = 0; y < yawDim; y++) {
             // Calculate the weight using the formula
-            weight[y][h] = (1 / (yawVar * Math.sqrt(2 * Math.PI))) * Math.exp(-Math.pow(y - yawDimCentre, 2) / (2 * Math.pow(yawVar, 2))) * (1 / (heightVar * Math.sqrt(2 * Math.PI))) * Math.exp(-Math.pow(h - heightDimCentre, 2) / (2 * Math.pow(heightVar, 2)))
+            weight[y][h] = (
+                (
+                    1 / (yawVar * Math.sqrt(2 * Math.PI))
+                ) * (
+                    Math.exp(
+                        -Math.pow(y - yawDimCentre, 2)
+                        /
+                        (2 * Math.pow(yawVar, 2))
+                    )
+                ) * (
+                    1 / (heightVar * Math.sqrt(2 * Math.PI))
+                ) * (
+                    Math.exp(
+                        -Math.pow(h - heightDimCentre, 2)
+                        /
+                        (2 * Math.pow(heightVar, 2))
+                    )
+                )
+            )
         }
     }
 
