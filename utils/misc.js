22f4d79cb68d34e94d5130ba46ce01e20c09a915
Jeff Hykin <jeff.hykin@gmail.com>
2025-02-05 18:23:08 -0600

    -

--- utils/misc.js
+++ utils/misc.js
@@ -1,5 +1,8 @@
 export function arrayOf({value, shape}) {
-    let output = new Array(shape.pop()).fill(value)
+    if (!(value instanceof Function)) {
+        value = () => value
+    }
+    let output = new Array(shape.pop()).fill(0).map((_, i) => value(i))
     while (shape.length > 0) {
         let nextSize = shape.pop()
         output = new Array(nextSize).fill(0).map(() => structuredClone(output))
