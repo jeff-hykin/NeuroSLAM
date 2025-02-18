ba4053b8dbfdbf99d4a4a7699ca474b9e371e3de
Jeff Hykin <jeff.hykin@gmail.com>
2025-02-18 12:43:53 -0600

    improve render

--- 03_visual_odometry/visual_odo_loop.js
+++ 03_visual_odometry/visual_odo_loop.js
@@ -146,6 +146,7 @@ export async function *visualOdoLoop({frames, groundTruthFile, odoGlobals}) {
                 diffHeightVImgs,
                 // globals
                 odoGlobals,
+                frame,
             }
             // 
             // plots
--- main.js
+++ main.js
@@ -7,18 +7,20 @@ import { zip, enumerate, count, permute, combinations, wrapAroundGet } from "htt
 import { mapToAsyncIterable } from "./utils/async.js"
 
 // increments of 10 for testing
-import uint8ArrayFor0001Png from "./01_NeuroSLAM_Datasets.ignore/02_SynPanData/0010.png.binaryified.js"
-import uint8ArrayFor0002Png from "./01_NeuroSLAM_Datasets.ignore/02_SynPanData/0020.png.binaryified.js"
-import uint8ArrayFor0003Png from "./01_NeuroSLAM_Datasets.ignore/02_SynPanData/0030.png.binaryified.js"
-import uint8ArrayFor0004Png from "./01_NeuroSLAM_Datasets.ignore/02_SynPanData/0040.png.binaryified.js"
-import uint8ArrayFor0005Png from "./01_NeuroSLAM_Datasets.ignore/02_SynPanData/0050.png.binaryified.js"
-import uint8ArrayFor0006Png from "./01_NeuroSLAM_Datasets.ignore/02_SynPanData/0060.png.binaryified.js"
-import uint8ArrayFor0007Png from "./01_NeuroSLAM_Datasets.ignore/02_SynPanData/0070.png.binaryified.js"
-import uint8ArrayFor0008Png from "./01_NeuroSLAM_Datasets.ignore/02_SynPanData/0080.png.binaryified.js"
-import uint8ArrayFor0009Png from "./01_NeuroSLAM_Datasets.ignore/02_SynPanData/0090.png.binaryified.js"
-import uint8ArrayFor0010Png from "./01_NeuroSLAM_Datasets.ignore/02_SynPanData/0100.png.binaryified.js"
+import uint8ArrayFor0001Png from "./01_NeuroSLAM_Datasets.ignore/02_SynPanData/0071.png.binaryified.js"
+import uint8ArrayFor0002Png from "./01_NeuroSLAM_Datasets.ignore/02_SynPanData/0072.png.binaryified.js"
+import uint8ArrayFor0003Png from "./01_NeuroSLAM_Datasets.ignore/02_SynPanData/0073.png.binaryified.js"
+import uint8ArrayFor0004Png from "./01_NeuroSLAM_Datasets.ignore/02_SynPanData/0074.png.binaryified.js"
+import uint8ArrayFor0005Png from "./01_NeuroSLAM_Datasets.ignore/02_SynPanData/0075.png.binaryified.js"
+import uint8ArrayFor0006Png from "./01_NeuroSLAM_Datasets.ignore/02_SynPanData/0076.png.binaryified.js"
+import uint8ArrayFor0007Png from "./01_NeuroSLAM_Datasets.ignore/02_SynPanData/0077.png.binaryified.js"
+import uint8ArrayFor0008Png from "./01_NeuroSLAM_Datasets.ignore/02_SynPanData/0078.png.binaryified.js"
+import uint8ArrayFor0009Png from "./01_NeuroSLAM_Datasets.ignore/02_SynPanData/0079.png.binaryified.js"
+import uint8ArrayFor0010Png from "./01_NeuroSLAM_Datasets.ignore/02_SynPanData/0080.png.binaryified.js"
 
-
+import uint8ArrayForSynPanMineCsv from "./02_NeuroSLAM_Groudtruth.ignore/syn_pan_mine.csv.binaryified.js"
+// import { parseCsv, createCsv } from "https://esm.sh/gh/jeff-hykin/good-js@1.14.3.1/source/csv.js"
+import { parseCsv, createCsv } from "https://esm.sh/gh/jeff-hykin/good-js@34eba35/source/csv.js"
 
 // 
 // render
@@ -79,8 +81,39 @@ import uint8ArrayFor0010Png from "./01_NeuroSLAM_Datasets.ignore/02_SynPanData/0
         uint8ArrayFor0009Png,
         uint8ArrayFor0010Png,
     ]
+    const csvText = new TextDecoder().decode(uint8ArrayForSynPanMineCsv)
+    const sheetData = parseCsv({input: csvText, firstRowIsColumnNames: true})
+    const frameToSheetData = new Map([
+        [uint8ArrayFor0001Png, sheetData[71-1]],
+        [uint8ArrayFor0002Png, sheetData[72-1]],
+        [uint8ArrayFor0003Png, sheetData[73-1]],
+        [uint8ArrayFor0004Png, sheetData[74-1]],
+        [uint8ArrayFor0005Png, sheetData[75-1]],
+        [uint8ArrayFor0006Png, sheetData[76-1]],
+        [uint8ArrayFor0007Png, sheetData[77-1]],
+        [uint8ArrayFor0008Png, sheetData[78-1]],
+        [uint8ArrayFor0009Png, sheetData[79-1]],
+        [uint8ArrayFor0010Png, sheetData[80-1]],
+    ])
     
+    let prevGroundTruth
+    let eachGroundTruth
+    let eachDelta = {}
     let frameIterator = mapToAsyncIterable(frameData, async (each)=>{
+        // set the ground truth
+        eachGroundTruth = {...frameToSheetData.get(each)}
+        for (const [key, value] of Object.entries(eachGroundTruth)) {
+            if (key-0 === key-0) {
+                delete eachGroundTruth[key]
+            }
+        }
+        if (prevGroundTruth) {
+            for (let eachKey of Object.keys(prevGroundTruth)) {
+                eachDelta[eachKey] = eachGroundTruth[eachKey] - prevGroundTruth[eachKey]
+            }
+        }
+        prevGroundTruth = eachGroundTruth
+
         // slow down rendering intentionally
         await new Promise(r=>setTimeout(r,1000))
         let newImageElement = mainVisualImage({pngData: each})
@@ -116,16 +149,19 @@ import uint8ArrayFor0010Png from "./01_NeuroSLAM_Datasets.ignore/02_SynPanData/0
         KEY_POINT_SET: [1644, 1741], 
         ODO_STEP: 1
     })
-    let frameIndex = 0
-    for await (let { transV, yawRotV, heightV, ...other } of visualOdoLoop({frames: frameIterator, odoGlobals: visualOdoGlobals})) {
-        frameIndex++
-
-        // 
+    
+    let iterIndex = 0
+    for await (let { transV, yawRotV, heightV, frame, ...other } of visualOdoLoop({frames: frameIterator, odoGlobals: visualOdoGlobals})) {
+        iterIndex++
+        
+        // add render data
         dataElement.innerText = JSON.stringify({
-            frameIndex,
+            iterIndex,
             transV,
             yawRotV,
             heightV,
+            groundTruth: eachGroundTruth,
+            eachDelta,
             // ...other
         }, null, 4)
     }
