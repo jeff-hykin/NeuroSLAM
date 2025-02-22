6661bd7facc1a0f5bbe853cb6c6959ad05ebd7c0
Jeff Hykin <jeff.hykin@gmail.com>
2025-02-21 18:14:49 -0600

    add other ground truth set

--- neo/ground_truth.js
+++ neo/ground_truth.js
@@ -1,10 +1,11 @@
 import uint8ArrayForSynPanMineCsv from "../02_NeuroSLAM_Groudtruth.ignore/syn_pan_mine.csv.binaryified.js"
+import uint8ArrayForSynPerMineCsv from "../02_NeuroSLAM_Groudtruth.ignore/syn_per_mine.csv.binaryified.js"
 // import { parseCsv, createCsv } from "https://esm.sh/gh/jeff-hykin/good-js@1.14.3.1/source/csv.js"
 import { parseCsv, createCsv } from "https://esm.sh/gh/jeff-hykin/good-js@34eba35/source/csv.js"
 
-const csvText = new TextDecoder().decode(uint8ArrayForSynPanMineCsv)
-export const synpan = parseCsv({input: csvText, firstRowIsColumnNames: true})
-for (let each of synpan) {
+export const synpan = parseCsv({input: new TextDecoder().decode(uint8ArrayForSynPanMineCsv), firstRowIsColumnNames: true})
+export const synper = parseCsv({input: new TextDecoder().decode(uint8ArrayForSynPerMineCsv), firstRowIsColumnNames: true})
+for (let each of synpan.concat(synper)) {
     each.toJSON = ()=>{
         let out = {}
         for (const [key, value] of Object.entries(each)) {
